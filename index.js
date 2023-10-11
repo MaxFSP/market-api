d0c37811dfaf78b6af04a33b3eab9d8595f40f47
Max <santiago.farfan2001@gmail.com>
2023-10-11 02:55:48 -0400

    updates

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ app.use(bodyparser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Include 'Content-Type' and other necessary headers
   next();
 });
-
 //test route
 app.get("/", (req, res, next) => {
   res.send("Hello World");
